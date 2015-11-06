5f562924986c9af239fc0c559ad0f7fe090cd4e3
Yinlong Su <suyinlong07@gmail.com>
2015-11-06 17:29:22 -0500

    New Configuration

--- src/edu/stonybrook/cs/ccdb/Configuration.java
+++ src/edu/stonybrook/cs/ccdb/Configuration.java
@@ -7,11 +7,11 @@ public class Configuration {
 	public static String PWD = "CSE532A2";
 	
 	public static String QUERY[] = {
-			"SELECT au.pid, au.name, s.pid, s.name FROM \"OrgAccount\" oa, na_user(oa) au, o_owner(oa) oo, signer(oo) s WHERE oa.balance-oa.limit<=1000 AND oa.balance-oa.limit>=-1000;",
-			"SELECT p.pid, p.name FROM \"Person\" p WHERE account_num(p)>=4 AND na_account_num(p)>=3;",
-			"SELECT DISTINCT a.number FROM \"Account\" a, o_owner(a) oo, signer(oo) s WHERE NOT max_limit(s) < 25000;",
-			"WITH RECURSIVE indirect_user(pid, name, number) AS ( SELECT aa.pid, aa.name, a.number FROM \"Account\" a, non_owner_a_user(a) aa UNION ALL SELECT aa.pid, aa.name, iu.number FROM indirect_user iu, \"Person\" p, \"Account\" a, non_owner_a_user(a) aa WHERE iu.pid=p.pid AND a.owner=p.pid) SELECT DISTINCT * FROM indirect_user ORDER by pid;",
-			"WITH RECURSIVE indirect_user(pid, name, number) AS ( SELECT aa.pid, aa.name, a.number FROM \"Account\" a, non_owner_a_user(a) aa UNION ALL SELECT aa.pid, aa.name, iu.number FROM indirect_user iu, \"Person\" p, \"Account\" a, non_owner_a_user(a) aa WHERE iu.pid=p.pid AND a.owner=p.pid) SELECT sum(DISTINCT a.balance) FROM  indirect_user iu, \"Account\" a WHERE iu.name='Joe'  AND iu.number=a.number;"
+			"SELECT au.id, au.name, s.id, s.name FROM \"Account\" oa, \"getAuthUsers\"(oa) au, \"getOrgOwner\"(oa) oo, \"getSigners\"(oo) s WHERE oa.balance - oa.limit <= 1000 AND oa.balance - oa.limit >= -1000;",
+			"SELECT p.id, p.name FROM \"Person\" p WHERE \"getAcctsNum\"(p)>=4 AND \"getAuthAcctsNum\"(p)>=3;",
+			"SELECT DISTINCT a.number FROM \"Account\" a, \"getOrgOwner\"(a) oo, \"getSigners\"(oo) s WHERE NOT \"getMaxLimit\"(s) < 25000;",
+			"WITH RECURSIVE indirect_user(id, name, number) AS ( SELECT aa.id, aa.name, a.number FROM \"Account\" a, \"getNonOwnerAuthUsers\"(a) aa UNION SELECT aa.id, aa.name, iu.number FROM indirect_user iu, \"Person\" p, \"Account\" a, \"getNonOwnerAuthUsers\"(a) aa WHERE iu.id = p.id AND a.owner = p.id) SELECT * FROM indirect_user;",
+			"WITH RECURSIVE indirect_user(id, name, number) AS ( SELECT aa.id, aa.name, a.number FROM \"Account\" a, \"getNonOwnerAuthUsers\"(a) aa UNION SELECT aa.id, aa.name, iu.number FROM indirect_user iu, \"Person\" p, \"Account\" a, \"getNonOwnerAuthUsers\"(a) aa WHERE iu.id = p.id AND a.owner = p.id) SELECT sum(a.balance) FROM indirect_user iu, \"Account\" a WHERE iu.name = 'Joe' AND iu.number = a.number;"
 	};
 
 }
