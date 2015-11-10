0f460c5cfe1dd154d989f334b1b0d15707f454cb
Yinlong Su <suyinlong07@gmail.com>
2015-11-09 19:50:17 -0500

    Change SQL queries 4 and 5

--- src/edu/stonybrook/cs/ccdb/Configuration.java
+++ src/edu/stonybrook/cs/ccdb/Configuration.java
@@ -18,8 +18,8 @@ public class Configuration {
 			"SELECT au.id, au.name, s.id, s.name FROM \"Account\" oa, \"getAuthUsers\"(oa) au, \"getOrgOwner\"(oa) oo, \"getSigners\"(oo) s WHERE oa.balance - oa.limit <= 1000 AND oa.balance - oa.limit >= -1000;",
 			"SELECT p.id, p.name FROM \"Person\" p WHERE \"getAcctsNum\"(p)>=4 AND \"getAuthAcctsNum\"(p)>=3;",
 			"SELECT DISTINCT a.number FROM \"Account\" a, \"getOrgOwner\"(a) oo, \"getSigners\"(oo) s WHERE NOT \"getMaxLimit\"(s) < 25000;",
-			"WITH RECURSIVE indirect_user(id, name, number) AS ( SELECT aa.id, aa.name, a.number FROM \"Account\" a, \"getNonOwnerAuthUsers\"(a) aa UNION SELECT aa.id, aa.name, iu.number FROM indirect_user iu, \"Person\" p, \"Account\" a, \"getNonOwnerAuthUsers\"(a) aa WHERE iu.id = p.id AND a.owner = p.id) SELECT * FROM indirect_user;",
-			"WITH RECURSIVE indirect_user(id, name, number) AS ( SELECT aa.id, aa.name, a.number FROM \"Account\" a, \"getNonOwnerAuthUsers\"(a) aa UNION SELECT aa.id, aa.name, iu.number FROM indirect_user iu, \"Person\" p, \"Account\" a, \"getNonOwnerAuthUsers\"(a) aa WHERE iu.id = p.id AND a.owner = p.id) SELECT sum(a.balance) FROM indirect_user iu, \"Account\" a WHERE iu.name = 'Joe' AND iu.number = a.number;"
+			"WITH RECURSIVE indirect_user(id, name, number) AS ( SELECT aa.id, aa.name, a.number FROM \"Account\" a, \"getNonOwnerAuthUsers\"(a) aa UNION SELECT aa.id, aa.name, iu.number FROM indirect_user iu, \"Account\" a, \"getNonOwnerAuthUsers\"(a) aa WHERE iu.id = a.owner) SELECT * FROM indirect_user;",
+			"WITH RECURSIVE indirect_user(id, name, number) AS ( SELECT aa.id, aa.name, a.number FROM \"Account\" a, \"getNonOwnerAuthUsers\"(a) aa UNION SELECT aa.id, aa.name, iu.number FROM indirect_user iu, \"Account\" a, \"getNonOwnerAuthUsers\"(a) aa WHERE iu.id = a.owner) SELECT sum(a.balance) FROM indirect_user iu, \"Account\" a WHERE iu.name = 'Joe' AND iu.number = a.number;"
 	};
 
 }
